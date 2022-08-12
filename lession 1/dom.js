5115f8a6f6782a81460f466df3be16fb81369e04
kythalcansadoape <kyth.ape1106@gmail.com>
2022-08-12 14:22:04 +0800

    meh

---
+++ lession 1/dom.js
@@ -0,0 +1,15 @@
+// console.dir(document)
+// ! examine dom
+
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.body)
+console.log(document.all)
+console.log(document.all[10])
+document.all[10].textContent = 'Hello'
+
+console.log(document.forms)
+
